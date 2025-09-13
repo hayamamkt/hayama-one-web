e40b9e0ced718ae404360921dd672bd5c1a27361
hayama <hayamaroom@gmail.com>
2025-09-13 14:42:21 +0800

    add daisyUI

--- projects/shell/src/app/app.ts
+++ projects/shell/src/app/app.ts
@@ -1,10 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
 @Component({
   selector: 'app-shell-root',
-  imports: [RouterOutlet, RouterLink],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  imports: [RouterOutlet, RouterLink, HeaderComponent],
+  template: `
+    <app-shell-header />
+    <router-outlet />
+  `
 })
 export class App {
   protected readonly title = signal('shell');
---
+++ projects/shell/src/app/components/header/header.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shell-header',
+  imports: [],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+
+}
