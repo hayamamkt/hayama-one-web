57b10912d6795d53551f84e9412094f7944d74ce
hayama <hayamaroom@gmail.com>
2025-09-13 15:13:24 +0800

    add theme

--- projects/shell/src/app/components/header/header.component.ts
+++ projects/shell/src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { ThemeCtrlComponent } from "../theme-ctrl/theme-ctrl.component";
 
 @Component({
   selector: 'app-shell-header',
-  imports: [RouterLink],
+  imports: [RouterLink, ThemeCtrlComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
