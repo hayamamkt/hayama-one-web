59215445c47f63f1f4c30bb017e2d7b08f1f17d3
hayama <hayamaroom@gmail.com>
2025-09-13 12:00:10 +0800

    add apps

--- projects/todos/src/app/app.ts
+++ projects/todos/src/app/app.ts
@@ -2,11 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-principal-root',
+  selector: 'app-todos-root',
   imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('principal');
+  protected readonly title = signal('todos');
 }
