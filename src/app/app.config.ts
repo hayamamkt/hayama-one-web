50ae65a96e115eae0c3067ac6dfacc6d0f9957ac
hayama <hayamaroom@gmail.com>
2025-09-17 18:00:44 +0800

    add @angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,11 +2,15 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessC
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
-    provideRouter(routes)
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "hayama-one-alt", appId: "1:985998555494:web:1b1505b0634052c08430a7", storageBucket: "hayama-one-alt.firebasestorage.app", apiKey: "AIzaSyAsWInVwR2eALZEbx0tvij8uv9hF339rvw", authDomain: "hayama-one-alt.firebaseapp.com", messagingSenderId: "985998555494", measurementId: "G-B5PP02VXNK" })), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideStorage(() => getStorage())
   ]
 };
