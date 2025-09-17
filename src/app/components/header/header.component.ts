7e1536cdb13b6a767584a65dfca76a41efed1521
hayama <hayamaroom@gmail.com>
2025-09-17 19:38:39 +0800

    add search btn

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ThemeCtrlComponent } from "../theme-ctrl/theme-ctrl.component";
+import { SearchBtnComponent } from "../search-btn/search-btn.component";
 
 @Component({
   selector: 'app-header',
-  imports: [ThemeCtrlComponent],
+  imports: [ThemeCtrlComponent, SearchBtnComponent],
   templateUrl: './header.component.html',
 })
 export class HeaderComponent {
